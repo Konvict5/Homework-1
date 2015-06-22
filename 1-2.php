9c6e5272d6592323da0af16631adca5ba9144756
Konvict5 <scorpioo@abv.bg>
2015-06-22 18:10:53 +0300

    Delete 1-2.php

--- 1-2.php
+++
@@ -1,7 +0,0 @@
-<?php
-	echo "Today is " . date("Y/m/d") "<br>";
-	echo "The time is " . date("h:i:sa");
-	date_default_timezone_set("America/New_York");
-	echo "The time is " . date("h:i:sa");
-	print "hi";
-	?>
