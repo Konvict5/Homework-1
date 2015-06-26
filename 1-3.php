189b69480706bf28bdba5785accee521b6abc72a
Konvict5 <scorpioo@abv.bg>
2015-06-26 18:44:40 +0300

    Homework 15.06

---
+++ 1-3.php
@@ -0,0 +1,14 @@
+<?php
+	$a= 7;
+	$as= 20;
+	print ($a * $as) . "<br>";
+	print ($a + $a + $as + $as) . "<br>";
+
+	$asd= 5;
+	$asdasd= 12;
+	print ($asd * $asdasd) . "<br>";
+	print ($asd*2 + $asdasd*2); 
+
+
+
+?>
